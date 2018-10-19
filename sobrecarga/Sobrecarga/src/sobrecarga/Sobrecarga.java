1e0fa8b02a0cf8568670d600d64b31c1c6459d1f
David <jhandry13@hotmail.com>
2018-10-19 10:40:34 -0500

    Agregando metodo agragar_sueldo con arreglo

--- sobrecarga/Sobrecarga/src/sobrecarga/Sobrecarga.java
+++ sobrecarga/Sobrecarga/src/sobrecarga/Sobrecarga.java
@@ -42,28 +42,34 @@ public class Sobrecarga {
         String apellidos = scanner.nextLine();
         t.agregar_apellidos(apellidos);
 
-        
-        System.out.println("Ingrese la frecuencia del pago \n 1. semanal \n 2. mensual");
+        System.out.println("Ingrese la frecuencia del pago \n 1. semanal \n 2. mensual \n 3. mensual(for)");
         int frecuencia = scanner.nextInt();
 
         if (frecuencia == 1) {
             System.out.println("Ingrese el valor del sueldo semanal");
             double sueldo = scanner.nextDouble();
             t.agregar_sueldo(sueldo);
-        }else{
-            System.out.println("Ingrese el valor del sueldo de la semana1");
-            double sueldo1 = scanner.nextDouble();
-            System.out.println("Ingrese el valor del sueldo de la semana2");
-            double sueldo2 = scanner.nextDouble();
-            System.out.println("Ingrese el valor del sueldo de la semana3");
-            double sueldo3 = scanner.nextDouble();
-            System.out.println("Ingrese el valor del sueldo de la semana4");
-            double sueldo4 = scanner.nextDouble();
-            
-            t.agrear_sueldo(sueldo1, sueldo2, sueldo3, sueldo4);
-            
-        }
+        } else {
+            if (frecuencia == 2) {
+                System.out.println("Ingrese el valor del sueldo de la semana1");
+                double sueldo1 = scanner.nextDouble();
+                System.out.println("Ingrese el valor del sueldo de la semana2");
+                double sueldo2 = scanner.nextDouble();
+                System.out.println("Ingrese el valor del sueldo de la semana3");
+                double sueldo3 = scanner.nextDouble();
+                System.out.println("Ingrese el valor del sueldo de la semana4");
+                double sueldo4 = scanner.nextDouble();
 
+                t.agrear_sueldo(sueldo1, sueldo2, sueldo3, sueldo4);
+
+            } else {
+                if (frecuencia == 3) {
+                    double sueldos[] = new double[4];
+                    t.agregar_sueldo(sueldos);
+                }
+            }
+
+        }
         System.out.printf("Los datos del trabajador son: %s\n", t.presentar_datos());
 
     }
--- sobrecarga/Sobrecarga/src/sobrecarga/Trabajador.java
+++ sobrecarga/Sobrecarga/src/sobrecarga/Trabajador.java
@@ -5,53 +5,65 @@
  */
 package sobrecarga;
 
+import java.util.Scanner;
+
 /**
  *
  * @author David Lopez
  */
 public class Trabajador {
+
     private String nombres;
     private double sueldo_mensual;
     private String apellidos;
-    
-    public void agregar_nombres(String n){
+    Scanner entrada = new Scanner(System.in);
+
+    public void agregar_nombres(String n) {
         nombres = n;
     }
-    
-    public void agregar_nombres(String n1, String n2){
+
+    public void agregar_nombres(String n1, String n2) {
         nombres = String.format("%s-%s", n1, n2);
     }
-    
-    
-    public void agregar_sueldo(double sueldo){
+
+    public void agregar_sueldo(double sueldo) {
         sueldo_mensual = sueldo;
     }
-    public void agrear_sueldo(double x, double y,double z,double a){
-        sueldo_mensual = x + y + z +a;
-        
+
+    public void agrear_sueldo(double x, double y, double z, double a) {
+        sueldo_mensual = x + y + z + a;
     }
-    
-        
-    public void agregar_apellidos(String n){
+
+    public void agregar_sueldo(double[] sueldos) {
+        double acm = 0;
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Ingrese el sueldo de la semana " + (i + 1));
+            sueldos[i] = entrada.nextDouble();
+            acm = acm + sueldos[i];
+        }
+        sueldo_mensual = acm;
+    }
+
+    public void agregar_apellidos(String n) {
         apellidos = n;
     }
-    
-    public String obtener_nombres(){
+
+    public String obtener_nombres() {
         return nombres;
     }
-    
-    public String obtener_apellidos(){
+
+    public String obtener_apellidos() {
         return apellidos;
     }
-    
-    public double obtener_sueldo_mensual(){
+
+    public double obtener_sueldo_mensual() {
         return sueldo_mensual;
     }
-    
-    public String presentar_datos(){
+
+    public String presentar_datos() {
         return String.format("\n\tNombres: %s \n\tApellidos: %s\n\t"
                 + "Sueldo: %.2f", obtener_nombres(), obtener_apellidos(),
                 obtener_sueldo_mensual());
     }
-    
+
 }
